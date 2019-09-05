3bd8a86b03fb55083d422de43eacb998ec5978a5
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-09-05 15:26:49 -0500

    v2.1
    Se consulta la firma del jar y se muestra en pantalla los certificados.

--- src/pruebafirma/PruebaFirma.java
+++ src/pruebafirma/PruebaFirma.java
@@ -5,7 +5,11 @@
  */
 package pruebafirma;
 
-import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,11 +17,26 @@ import javax.swing.JOptionPane;
  */
 public class PruebaFirma {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Prueba");
+    static public void main(String[] args) {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            String comando = "jarsigner -verify -verbose -certs " + "dist/PruebaFirma.jar";
+            String[] llamada = {"cmd.exe", "/C", comando};
+            
+            System.out.println("llamando comando: " + comando);
+
+            Process proc = rt.exec(llamada);
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+            String s;
+            
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(PruebaFirma.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
