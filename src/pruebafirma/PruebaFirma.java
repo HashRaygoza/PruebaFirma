ba1b600b098e858038ea1c25961653b07aa9ea3b
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-09-05 16:54:50 -0500

    v2.5
    Los datos relevantes del certificado se almacenan en el JAR y se muestran en pantalla al ejecutar el jar

--- src/pruebafirma/PruebaFirma.java
+++ src/pruebafirma/PruebaFirma.java
@@ -7,9 +7,11 @@ package pruebafirma;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,24 +21,31 @@ public class PruebaFirma {
 
     static public void main(String[] args) {
         try {
-            Runtime rt = Runtime.getRuntime();
-            String comando = "jarsigner -verify -verbose -certs " + "dist/PruebaFirma.jar";
-            String[] llamada = {"cmd.exe", "/C", comando};
+            PruebaFirma prueba = new PruebaFirma();
             
-            System.out.println("llamando comando: " + comando);
-
-            Process proc = rt.exec(llamada);
-
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-
-            String s;
+            String mensaje = prueba.leerCert();
             
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-
+            JOptionPane.showMessageDialog(null, mensaje);
+            
         } catch (IOException ex) {
             Logger.getLogger(PruebaFirma.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
+        
+    }
+
+    public String leerCert() throws IOException {
+        InputStream is = getClass().getResourceAsStream("/dataCert.txt");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String mensaje = "";
+        String cadena;
+        
+        while( (cadena = br.readLine()) != null) {
+            mensaje = mensaje + cadena + "\n";
+        }
+        
+        return mensaje;
     }
 }
